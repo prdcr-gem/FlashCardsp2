4c294c4f713d3b328a3a5e991344a5b9068e6a86
CYB101NAME <CYB101EMAIL>
2025-03-16 23:08:58 -0400

    import issue resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { Card, CardContent } from "../components/ui/card";
-import { Button } from "../components/ui/button";
+import { Card, CardContent } from "./components/ui/card";
+import { Button } from "./components/ui/button";
 
 
 const flashcards = [
